521da81198542759796b77f456bd27d80f0a2e0d
ubernostrum <ubernostrum@d6b9e1ad-042d-0410-a639-15a354c1509c>
2007-04-02 23:36:14 +0000

    Make markup template filter take filter_name argument
    git-svn-id: http://django-template-utils.googlecode.com/svn/trunk@23 d6b9e1ad-042d-0410-a639-15a354c1509c

--- templatetags/generic_markup.py
+++ templatetags/generic_markup.py
@@ -6,11 +6,15 @@ A filter which can perform many types of text-to-HTML conversion.
 from django.template import Library
 from template_utils.markup import markup_filter
 
-def apply_markup(value):
+def apply_markup(value, arg=None):
     """
     Applies text-to-HTML conversion.
 
+    Takes an optional argument to specify the name of a filter to use.
+
     """
+    if arg is not None:
+        return markup_filter(value, filter_name=arg)
     return markup_filter(value)
 
 register = Library()

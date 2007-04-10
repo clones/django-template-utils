a45c9e527cbe14ef6252acef512f3b6f9a8a65a4
ubernostrum <ubernostrum@d6b9e1ad-042d-0410-a639-15a354c1509c>
2007-04-10 06:53:19 +0000

    Add reST-formatted documentation
    git-svn-id: http://django-template-utils.googlecode.com/svn/trunk@47 d6b9e1ad-042d-0410-a639-15a354c1509c

--- template_utils/templatetags/public_comments.py
+++ template_utils/templatetags/public_comments.py
@@ -6,9 +6,25 @@ moderation.
 
 from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import get_model
-from django.template import Library
-from django.contrib.comments.templatetags.comments import CommentListNode
+from django import template
+from django.contrib.comments.models import Comment, FreeComment
+from django.contrib.comments.templatetags.comments import CommentCountNode, CommentListNode
 from django.contrib.contenttypes.models import ContentType
+
+
+class PublicCommentCountNode(CommentCountNode):
+    def render(self, context):
+        from django.conf import settings
+        manager = self.free and FreeComment.objects or Comment.objects
+        if self.context_var_name is not None:
+            object_id = template.resolve_variable(self.context_var_name, context)
+        comment_count = manager.filter(object_id__exact=object_id,
+                                       content_type__app_label__exact=self.package,
+                                       content_type__model__exact=self.module,
+                                       site__id__exact=settings.SITE_ID,
+                                       is_public__exact=True).count()
+        context[self.var_name] = comment_count
+        return ''
 
 
 class DoPublicCommentList(object):
@@ -31,9 +47,9 @@ class DoPublicCommentList(object):
     
         {% get_public_free_comment_list for [app_name].[model_name] [object_id] as [varname] %}
     
-    When called using ``get_public_comment_list``, this tag retrieves
-    instances of ``Comment`` (e.g., comments which require
-    registration). When called using ``get_public_free_comment_list``,
+    When called as ``get_public_comment_list``, this tag retrieves
+    instances of ``Comment`` (comments which require
+    registration). When called as ``get_public_free_comment_list``,
     this tag retrieves instances of ``FreeComment`` (comments which do
     not require registration).
     
@@ -75,7 +91,7 @@ class DoPublicCommentList(object):
         if bits[3].isdigit():
             object_id = bits[3]
             try:
-                content_type.get_object_for_this_type(pk=obj_id)
+                content_type.get_object_for_this_type(pk=object_id)
             except ObjectDoesNotExist:
                 raise template.TemplateSyntaxError("'%s' tag got reference to %s object with id %s, which doesn't exist" % (bits[0], content_type.name, object_id))
         else:
@@ -90,6 +106,71 @@ class DoPublicCommentList(object):
             ordering = ''
         return CommentListNode(app_name, model_name, var_name, object_id, bits[5], self.free, ordering, extra_kwargs={ 'is_public__exact': True })
 
-register = Library()
+
+class DoPublicCommentCount(object):
+    """
+    Retrieves the number of comments attached to a particular object
+    and stores them in a context variable.
+
+    The difference between this tag and Django's built-in comment
+    count tags is that this tag will only count comments with
+    ``is_public=True``. If your application uses any sort of comment
+    moderation which sets ``is_public=False``, you'll probably want to
+    use this tag, as it gives an accurate count of the comments which
+    will be publicly displayed.
+    
+    Syntax::
+    
+        {% get_public_comment_count for [app_name].[model_name] [object_id] as [varname] %}
+    
+    or::
+    
+        {% get_public_free_comment_count for [app_name].[model_name] [object_id] as [varname] %}
+    
+    Example::
+
+        {% get_public_comment_count for weblog.entry entry.id as comment_count %}
+
+    When called as ``get_public_comment_list``, this tag counts
+    instances of ``Comment`` (comments which require
+    registration). When called as ``get_public_free_comment_count``,
+    this tag counts instances of ``FreeComment`` (comments which do
+    not require registration).
+
+    """
+    def __init__(self, free):
+        self.free = free
+
+    def __call__(self, parser, token):
+        bits = token.contents.split()
+        if len(bits) != 6:
+            raise template.TemplateSyntaxError("'%s' tag takes five arguments" % bits[0])
+        if bits[1] != 'for':
+            raise template.TemplateSyntaxError("first argument to '%s' tag must be 'for'" % bits[0])
+        try:
+            app_name, model_name = bits[2].split('.')
+        except ValueError:
+            raise template.TemplateSyntaxError("second argument to '%s tag must be in the format app_name.model_name'" % bits[0])
+        model = get_model(app_name, model_name)
+        if model is None:
+            raise template.TemplateSyntaxError("'%s' tag got invalid model '%s.%s'" % (app_name, model_name))
+        content_type = ContentType.objects.get_for_model(model)
+        var_name, object_id = None, None
+        if bits[3].isdigit():
+            object_id = bits[3]
+            try:
+                content_type.get_object_for_this_type(pk=object_id)
+            except ObjectDoesNotExist:
+                raise template.TemplateSyntaxError("'%s' tag got reference to %s object with id %s, which doesn't exist" % (bits[0], content_type.name, object_id))
+        else:
+            var_name = bits[3]
+        if bits[4] != 'as':
+            raise template.TemplateSyntaxError("fourth argument to '%s' tag must be 'as'" % bits[0])
+        return PublicCommentCountNode(app_name, model_name, var_name, object_id, bits[5], self.free)
+
+
+register = template.Library()
 register.tag('get_public_comment_list', DoPublicCommentList(False))
 register.tag('get_public_free_comment_list', DoPublicCommentList(True))
+register.tag('get_public_comment_count', DoPublicCommentCount(False))
+register.tag('get_public_free_comment_count', DoPublicCommentCount(True))

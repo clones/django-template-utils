d10ecfe9a19fcf858fd7f03ba354f27ba62d36d4
ubernostrum <ubernostrum@d6b9e1ad-042d-0410-a639-15a354c1509c>
2007-07-05 07:01:02 +0000

    Make Textile filter work again now that Django's Unicode branch has merged.
    git-svn-id: http://django-template-utils.googlecode.com/svn/trunk@82 d6b9e1ad-042d-0410-a639-15a354c1509c

--- template_utils/markup.py
+++ template_utils/markup.py
@@ -6,10 +6,19 @@ Utilities for text-to-HTML conversion.
 def textile(text, **kwargs):
     """
     Applies Textile conversion to a string, and returns the HTML.
+
+    Note that PyTextile does not handle Unicode strings correctly, so
+    this filter uses Django's ``force_unicode`` and ``smart_str``
+    utilities to roundtrip to a utf-8 bytestring and back to Unicode
+    when needed.
+
+    If you're not using Django, you'll need to supply your own Textile
+    filter with any necessary workarounds for Unicode strings.
     
     """
+    from django.utils.encoding import smart_str, force_unicode
     import textile
-    return textile.textile(text, **kwargs)
+    return force_unicode(textile.textile(smart_str(text), encoding='utf-8', output='utf-8'))
 
 def markdown(text, **kwargs):
     """

b9e0b13451b7b5435f3697098a34a235e05c284b
ubernostrum <ubernostrum@d6b9e1ad-042d-0410-a639-15a354c1509c>
2007-07-25 07:49:58 +0000

    Preliminary consolidation into GenericContentNode
    git-svn-id: http://django-template-utils.googlecode.com/svn/trunk@84 d6b9e1ad-042d-0410-a639-15a354c1509c

--- template_utils/templatetags/generic_content.py
+++ template_utils/templatetags/generic_content.py
@@ -5,35 +5,70 @@ Template tags which can do retrieval of content from any model.
 
 
 from django import template
+from django.conf import settings
 from django.db.models import get_model
 
 
-class LatestObjectsNode(template.Node):
+class GenericContentNode(template.Node):
+    """
+    Base Node class for retrieving objects from any model.
+
+    By itself, this class will retrieve a number of objects from a
+    particular model (specified by an "app_name.model_name" string)
+    and store them in a specified context variable (these are the
+    ``num``, ``model`` and ``varname`` arguments to the constructor,
+    respectively), but is also intended to be subclassed for
+    customization.
+
+    There are two ways to add extra bits to the eventual database lookup:
+
+    1. Add the setting ``GENERIC_CONTENT_LOOKUP_KWARGS`` to your
+       settings file; this should be a dictionary whose keys are
+       "app_name.model_name" strings correponding to models, and whose
+       values are dictionaries of keyword arguments which will be
+       passed to ``filter()``.
+
+    2. Subclass and override ``_get_query_set``; all that's expected
+       is that it will return a ``QuerySet`` or ``None``; if it
+       returns a ``QuerySet``, that will be used to get the object(s)
+       to add to the template context. The default ``QuerySet`` for
+       the specified model (filtered as described above) will be
+       available as ``self.query_set`` if you want to work with it.
+    
+    """
     def __init__(self, model, num, varname):
         self.model, self.num, self.varname = model, int(num), varname
+        lookup_dict = getattr(settings, 'GENERIC_CONTENT_LOOKUP_KWARGS', {})
+        model = get_model(*self.model.split('.'))
+        lookup_kwargs = lookup_dict.get(self.model, {})
+        if model is not None:
+            self.query_set = model._default_manager.filter(**lookup_kwargs)
+        else:
+            self.query_set = model
+        
+    def _get_query_set(self):
+        return self.query_set
     
     def render(self, context):
-        model = get_model(*self.model.split('.'))
-        if model is not None:
+        query_set = self._get_query_set()
+        if query_set is not None:
             if self.num == 1:
-                context[self.varname] = model._default_manager.all()[0]
+                context[self.varname] = query_set[0]
             else:
-                context[self.varname] = list(model._default_manager.all()[:self.num])
+                context[self.varname] = list(query_set[:self.num])
         return ''
 
 
-class RandomObjectsNode(template.Node):
-    def __init__(self, model, num, varname):
-        self.model, self.num, self.varname = model, int(num), varname
+class RandomObjectsNode(GenericContentNode):
+    """
+    A subclass of ``GenericContentNode`` which overrides
+    ``_get_query_set`` to apply random ordering.
     
-    def render(self, context):
-        model = get_model(*self.model.split('.'))
-        if model is not None:
-            if self.num == 1:
-                context[self.varname] = model._default_manager.order_by('?')[0]
-            else:
-                context[self.varname] = list(model._default_manager.order_by('?')[:self.num])
-        return ''
+    """
+    def _get_query_set(self):
+        if self.query_set is not None:
+            self.query_set = self.query_set.order_by('?')
+        return self.query_set
 
 
 class RetrieveObjectNode(template.Node):
@@ -41,6 +76,9 @@ class RetrieveObjectNode(template.Node):
         self.model, self.pk, self.varname = model, pk, varname
     
     def render(self, context):
+        # PK lookup assumes we know exactly which object we want,
+        # so the extra settings-based filtering applied in the other
+        # tags is not used here.
         model = get_model(*self.model.split('.'))
         if model is not None:
             try:
@@ -69,7 +107,8 @@ def do_latest_object(parser, token):
         raise template.TemplateSyntaxError("'%s' tag takes three arguments" % bits[0])
     if bits [2] != 'as':
         raise template.TemplateSyntaxError("second argument to '%s' tag must be 'as'" % bits[0])
-    return LatestObjectsNode(bits[1], 1, bits[3])
+    return GenericContentNode(bits[1], 1, bits[3])
+
 
 def do_latest_objects(parser, token):
     """
@@ -90,7 +129,7 @@ def do_latest_objects(parser, token):
         raise template.TemplateSyntaxError("'%s' tag takes four arguments" % bits[0])
     if bits [3] != 'as':
         raise template.TemplateSyntaxError("third argument to '%s' tag must be 'as'" % bits[0])
-    return LatestObjectsNode(bits[1], bits[2], bits[4])
+    return GenericContentNode(bits[1], bits[2], bits[4])
 
 def do_random_object(parser, token):
     """
@@ -113,6 +152,7 @@ def do_random_object(parser, token):
         raise template.TemplateSyntaxError("second argument to '%s' tag must be 'as'" % bits[0])
     return RandomObjectsNode(bits[1], 1, bits[3])
 
+
 def do_random_objects(parser, token):
     """
     Retrieves ``num`` random objects from a given model, and stores
@@ -133,6 +173,7 @@ def do_random_objects(parser, token):
     if bits [3] != 'as':
         raise template.TemplateSyntaxError("third argument to '%s' tag must be 'as'" % bits[0])
     return RandomObjectsNode(bits[1], bits[2], bits[4])
+
 
 def do_retrieve_object(parser, token):
     """
